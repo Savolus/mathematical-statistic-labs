aa37f625d3281a7e478d10082233928a0e1ed1c9
Mykola Dorohyi <mdorohyi@c1r6p9.khpi.ucode-connect.study>
2021-05-18 20:35:15 +0300

    Fix rm

--- sprint-01/js/variance.js
+++
@@ -1,7 +0,0 @@
-const varianceStandart = (sample, mean) => sample.reduce((acc, value) => acc += ((value - mean) ** 2), 0) / sample.length
-const varianceCorrected = (sample, mean) => sample.reduce((acc, value) => acc += ((value - mean) ** 2), 0) / (sample.length - 1)
-
-export {
-    varianceStandart,
-    varianceCorrected
-}
